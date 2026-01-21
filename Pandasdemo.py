07f9ef5f5e6ee02a9afd9658bb15d4ded12b73c0
Nethra G S <nethra120304@gmail.com>
2026-01-21 15:46:54 +0530

    pandas,numpy added

---
+++ Pandasdemo.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+print(df) # Display the entire DataFrame
+print(df.head()) # Display first 5 rows
+print(df.tail()) # Display last 5 rows
+print(df.columns) # Display column names
+print(df.info()) # Display summary information
+print(df.describe()) # Display statistical summary
